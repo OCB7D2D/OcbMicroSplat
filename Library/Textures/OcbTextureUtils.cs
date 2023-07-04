9158679e75add3810714cb222803e7297d44f82f
Marcel Greter <marcel.greter@ocbnet.ch>
2023-07-04 23:33:07 +0200

    Add shared utility classes to use later

---
+++ Library/Textures/OcbTextureUtils.cs
@@ -0,0 +1,60 @@
+﻿/* MIT License
+
+Copyright (c) 2022-2023 OCB7D2D
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public static class OcbTextureUtils
+{
+
+    // ####################################################################
+    // ####################################################################
+
+    // Blit texture from GPU into RenderTexture and copy pixels back
+    public static Texture2D TextureFromGPU(Texture src, int idx, bool linear)
+    {
+        if (src == null) return null;
+        // Create render texture target
+        var fmt = SystemInfo.GetCompatibleFormat(
+            src.graphicsFormat, FormatUsage.Render);
+        RenderTexture rt = RenderTexture.GetTemporary(
+            src.width, src.height, 32, fmt);
+        RenderTexture active = RenderTexture.active;
+        Graphics.Blit(src, rt, idx, 0);
+        // Create CPU texture to hold the pixels
+        Texture2D tex = new Texture2D(src.width, src.height,
+            fmt, src.mipmapCount, TextureCreationFlags.MipChain);
+        // Read pixels from current render texture as set by Blit 
+        tex.ReadPixels(new Rect(0, 0, src.width, src.height), 0, 0, true);
+        // Apply new pixels to CPU texture
+        tex.Apply(false, false);
+        RenderTexture.active = active;
+        RenderTexture.ReleaseTemporary(rt);
+        return tex;
+    }
+
+    // ####################################################################
+    // ####################################################################
+
+}
---
+++ Library/Utils/HarmonyUtils.cs
@@ -0,0 +1,64 @@
+﻿/* MIT License
+
+Copyright (c) 2022-2023 OCB7D2D
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+using HarmonyLib;
+using System.Collections;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+
+public static class HarmonyUtils
+{
+    // Our AccessTools is too old and doesn't have this
+    // Modern HarmonyX has `AccessTool.EnumeratorMoveNext`
+    public static MethodInfo GetEnumeratorMoveNext(MethodBase method)
+    {
+        if (method is null)
+        {
+            Log.Out("AccessTools.EnumeratorMoveNext: method is null");
+            return null;
+        }
+
+        var codes = PatchProcessor.ReadMethodBody(method).Where(pair => pair.Key == OpCodes.Newobj);
+        if (codes.Count() != 1)
+        {
+            Log.Out($"AccessTools.EnumeratorMoveNext: {method.FullDescription()} contains no Newobj opcode");
+            return null;
+        }
+        var ctor = codes.First().Value as ConstructorInfo;
+        if (ctor == null)
+        {
+            Log.Out($"AccessTools.EnumeratorMoveNext: {method.FullDescription()} contains no constructor");
+            return null;
+        }
+        var type = ctor.DeclaringType;
+        if (type == null)
+        {
+            Log.Out($"AccessTools.EnumeratorMoveNext: {method.FullDescription()} refers to a global type");
+            return null;
+        }
+        return AccessTools.Method(type, nameof(IEnumerator.MoveNext));
+    }
+
+}
