10f33c1d3b600d43f299cb24687ff62e4be66895
Marcel Greter <marcel.greter@ocbnet.ch>
2023-10-01 22:28:22 +0200

    Fix transpiler patch not working sometimes

--- Harmony/MicroSplatBiomeColors.cs
+++ Harmony/MicroSplatBiomeColors.cs
@@ -1,92 +1,89 @@
 ﻿using HarmonyLib;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Xml.Linq;
+using Unity.Collections;
 using UnityEngine;
-
-[HarmonyPatch]
-static class GenerateWorldFromRawInitPatch
+static class ChunkProviderBiomeColorsPatch
 {
 
-    // ####################################################################
-    // Draw additional biomes into splat maps according to
-    // settings in `MicroSplatXmlConfig.GetWorldConfig(name)`
-    // This can be different according to the map `name`
-    // ####################################################################
+    static ChunkProviderGenerateWorldFromRaw generateWorldFromRaw = null;
 
-    // Select the target dynamically to patch `MoveNext`
-    // Coroutine/Enumerator is compiled to a hidden class
-    static IEnumerable<MethodBase> TargetMethods()
+    private static readonly HarmonyFieldProxy<IBiomeProvider> BiomeProviderField =
+        new HarmonyFieldProxy<IBiomeProvider>(typeof(ChunkProviderGenerateWorld), "m_BiomeProvider");
+
+    [HarmonyPatch(typeof(ChunkProviderGenerateWorldFromRaw), "Init")]
+    static class CaptureChunkProviderOnInitForLater
     {
-        yield return HarmonyUtils.GetEnumeratorMoveNext(AccessTools.
-            Method(typeof(ChunkProviderGenerateWorldFromRaw), "Init"));
+        static void Prefix(ChunkProviderGenerateWorldFromRaw __instance)
+            => generateWorldFromRaw = __instance;
     }
 
-
-    // ####################################################################
-    // ####################################################################
-
-    // Function will be executed at the patched position
-    static void ExecutePatched(ref Color32 col1, ref Color32 col2, BiomeDefinition biome)
+    [HarmonyPatch(typeof(Log), "Out", new System.Type[] { typeof(string) })]
+    static class PatchProcBiomeMaskAfterVanillaPatching
     {
-        // Note: this is very much "sub-optimized"
-        // Should optimize fetching of world config
-        // Given that it only runs on init, kinda ok!?
-        var config = OcbMicroSplat.Config.MicroSplatWorldConfig;
-        if (config.BiomeColors.TryGetValue(biome.m_Id,
-            out MicroSplatBiomeColor value))
+        static void Postfix(string _txt)
         {
-            col1 = value.Color1;
-            col2 = value.Color2;
-        }
-    }
-
-    // ####################################################################
-    // ####################################################################
-
-    // Main function handling the IL patching
-    static IEnumerable<CodeInstruction> Transpiler(
-        IEnumerable<CodeInstruction> instructions)
-    {
-        var codes = new List<CodeInstruction>(instructions);
-        for (int i = 0; i < codes.Count; i++)
-        {
-            // Search for first color assignment
-            if (codes[i].opcode != OpCodes.Ldloca_S) continue;
-            if (!(codes[i].operand is LocalBuilder col1)) continue;
-            if (!typeof(Color32).IsAssignableFrom(col1.LocalType)) continue;
-            if (++i >= codes.Count) break;
-            if (codes[i].opcode != OpCodes.Initobj) continue;
-            if (++i >= codes.Count) break;
-            // Search for second color assignment
-            if (codes[i].opcode != OpCodes.Ldloca_S) continue;
-            if (!(codes[i].operand is LocalBuilder col2)) continue;
-            if (!typeof(Color32).IsAssignableFrom(col2.LocalType)) continue;
-            if (++i >= codes.Count) break;
-            if (codes[i].opcode != OpCodes.Initobj) continue;
-            if (++i >= codes.Count) break;
-            // Search for future assignment
-            for (int j = i; j < i + 20; j++)
+            if (generateWorldFromRaw == null) return;
+            // This is a very bad and mad way of patching stuff, but the transpiler
+            // below tends to give headaches due to a transpiler bug with HarmonyX.
+            // Overhead should be ok, given that logging is already quite expensive!
+            if (_txt.StartsWith("Loading and creating shader control textures took"))
             {
-                if (codes[j].opcode != OpCodes.Ldloc_S) continue;
-                if (!(codes[j].operand is LocalBuilder biome)) continue;
-                if (!typeof(BiomeDefinition).IsAssignableFrom(biome.LocalType)) continue;
-
-                codes.Insert(i++, new CodeInstruction(OpCodes.Ldloca_S, col1));
-                codes.Insert(i++, new CodeInstruction(OpCodes.Ldloca_S, col2));
-                codes.Insert(i++, new CodeInstruction(OpCodes.Ldloc_S, biome));
-                codes.Insert(i++, CodeInstruction.Call(
-                    typeof(GenerateWorldFromRawInitPatch),
-                    "ExecutePatched"));
-                return codes;
+                MicroStopwatch ms = new MicroStopwatch();
+                int width = generateWorldFromRaw.GetWorldSize().x / 8;
+                int height = generateWorldFromRaw.GetWorldSize().y / 8;
+                generateWorldFromRaw.procBiomeMask1 = new Texture2D(width, height, TextureFormat.RGBA32, false);
+                generateWorldFromRaw.procBiomeMask2 = new Texture2D(width, height, TextureFormat.RGBA32, false);
+                NativeArray<Color32> pixelData1 = generateWorldFromRaw.procBiomeMask1.GetPixelData<Color32>(0);
+                NativeArray<Color32> pixelData2 = generateWorldFromRaw.procBiomeMask2.GetPixelData<Color32>(0);
+                generateWorldFromRaw.GetWorldExtent(out Vector3i _minSize, out Vector3i _maxSize);
+                var biomeProvider = BiomeProviderField.Get(generateWorldFromRaw);
+                var config = OcbMicroSplat.Config.MicroSplatWorldConfig;
+                for (int z = _minSize.z; z < _maxSize.z; z += 8)
+                {
+                    int num = (z - _minSize.z) / 8 * width;
+                    for (int x = _minSize.x; x < _maxSize.x; x += 8)
+                    {
+                        if (biomeProvider.GetBiomeAt(x, z) is BiomeDefinition biome)
+                        {
+                            Color32 color32_1 = new Color32();
+                            Color32 color32_2 = new Color32();
+                            int index = (x - _minSize.x) / 8 + num;
+                            switch (biome.m_Id)
+                            {
+                                case 1:
+                                    color32_1.r = byte.MaxValue;
+                                    break;
+                                case 3:
+                                    color32_1.g = byte.MaxValue;
+                                    break;
+                                case 5:
+                                    color32_2.r = byte.MaxValue;
+                                    break;
+                                case 8:
+                                    color32_1.a = byte.MaxValue;
+                                    break;
+                                case 9:
+                                    color32_1.b = byte.MaxValue;
+                                    break;
+                            }
+                            if (config.BiomeColors.TryGetValue(biome.m_Id,
+                                out MicroSplatBiomeColor value))
+                            {
+                                color32_1 = value.Color1;
+                                color32_2 = value.Color2;
+                            }
+                            pixelData1[index] = color32_1;
+                            pixelData2[index] = color32_2;
+                        }
+                    }
+                }
+                generateWorldFromRaw.procBiomeMask1.filterMode = FilterMode.Bilinear;
+                generateWorldFromRaw.procBiomeMask1.Apply(false, true);
+                generateWorldFromRaw.procBiomeMask2.filterMode = FilterMode.Bilinear;
+                generateWorldFromRaw.procBiomeMask2.Apply(false, true);
+                Log.Out("Re-creating shader control textures took " + ms.ElapsedMilliseconds.ToString() + "ms");
+                generateWorldFromRaw = null;
             }
-            break;
         }
-        return codes;
     }
-
-    // ####################################################################
-    // ####################################################################
-
 }
+
